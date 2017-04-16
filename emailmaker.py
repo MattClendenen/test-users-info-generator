6e1ce50f7ce0c75ddf1cf2e663683b4868795430
Matt Clendenen <Matt Clendenen>
2017-04-15 23:07:25 -0400

    second

--- emailmaker.py
+++ emailmaker.py
@@ -2,39 +2,50 @@ import tempfile
 import random
 import string
 
-numFirst = raw_input("How many first names : ")
-numLast = raw_input("How many last names? : ")
+# define main
+def main():
+    numFirst = raw_input("How many first names : ")
+    numLast = raw_input("How many last names? : ")
 
-temp = tempfile.TemporaryFile(mode="w+b")
+    temp = tempfile.TemporaryFile(mode="w+b")
 
-lastFile = open('last.txt', 'r+')
+    lastFile = open('last.txt', 'r+')
 
-maleFirstFile = open('malefirst.txt', 'r+')
-femaleFirstFile = open('femalefirst.txt', 'r+')
-
-
-files_first = [maleFirstFile, femaleFirstFile]
-
+    maleFirstFile = open('malefirst.txt', 'r+')
+    femaleFirstFile = open('femalefirst.txt', 'r+')
 
 
-for i in range(0, int(numFirst)):
-    first = random.choice(files_first).readline().split(' ')[0]
-    for j in range(0, int(numLast)):
-        last = lastFile.readline().split(' ')[0]
-        temp.write(first + " " + last + "\n")
-    lastFile.seek(0)
-
-maleFirstFile.close()
-femaleFirstFile.close()
-lastFile.close()
-
-temp.seek(0)
-
-emailFile = open('emails.txt', 'w+')
-
-for line in temp:
-    emailFile.write(line.rstrip()+"\n")
+    files_first = [maleFirstFile, femaleFirstFile]
 
 
-emailFile.close()
-temp.close()
+
+    for i in range(0, int(numFirst)):
+        first = random.choice(files_first).readline().split(' ')[0]
+        for j in range(0, int(numLast)):
+            last = lastFile.readline().split(' ')[0]
+            temp.write(first + " " + last + "\n")
+        lastFile.seek(0)
+
+    maleFirstFile.close()
+    femaleFirstFile.close()
+    lastFile.close()
+
+    temp.seek(0)
+
+    emailFile = open('emails.txt', 'w+')
+
+    for line in temp:
+        emailFile.write(line.rstrip()+"\n")
+
+
+    emailFile.close()
+    temp.close()
+
+# define generate
+
+def generate():
+
+
+# execute
+if __name__ == "__main__":
+    main()

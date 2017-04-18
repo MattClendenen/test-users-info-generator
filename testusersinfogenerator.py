f1efa8d37b1a91221afd5e234b9f37a4c8939d34
Matt Clendenen <Matt Clendenen>
2017-04-18 00:06:01 -0400

    list of first name files

--- testusersinfogenerator.py
+++ testusersinfogenerator.py
@@ -3,20 +3,29 @@ from random import *
 from string import *
 from argparse import *
 
+
+# # # # # # # # # # # # # # # # # # #
+#                                   #
+#       Function Definitions        #
+#                                   #
+# # # # # # # # # # # # # # # # # # #
+
 # define main
 def main():
 
     # buld command line argument parser
     parser = build_parser()
-
     # parse command line arguments
     args = parser.parse_args()
 
-    # if | --help -h | print usage -> exit
-    if args.help:
-        print args
-
-    # which file for first names
+    # conditions based on arguments
+    if args.text:
+        exportFileName = 'testusersinfo.txt'
+        delimiter = ' '
+    else:
+        exportFileName = 'testusersinfo.csv'
+        delimiter = ','
+    ## which file for first names
     if args.male:
         which_file = all_males_file
     elif args.female:
@@ -30,43 +39,43 @@ def main():
 
     # open files
     tempFile = TemporaryFile(mode="w+b")
-
     lastFile = open('last.txt', 'r+')
-
     maleFirstFile = open('malefirst.txt', 'r+')
     femaleFirstFile = open('femalefirst.txt', 'r+')
 
+    # list of first name files
+    listOpenFiles = [maleFirstFile, femaleFirstFile]
+
     # generate
-    generate(numFirst, numLast, maleFirstFile, femaleFirstFile, lastFile, tempFile, which_file)
+    generate(numFirst, numLast, listOpenFiles, lastFile, tempFile, which_file , delimiter)
 
     # close name files
-    maleFirstFile.close()
-    femaleFirstFile.close()
+    for openFile in listOpenFiles:
+        openFile.close()
     lastFile.close()
 
     # rewind temp file
     tempFile.seek(0)
 
-    # open real file
-    emailFile = open('emails.txt', 'w+')
+    # open real export file
+    exportFile = open(exportFileName, 'w+')
 
     # copy to temp to real file
     for line in tempFile:
-        emailFile.write(line.rstrip()+"\n")
+        exportFile.write(line.rstrip()+"\n")
 
 
     # close temp and real file
-    emailFile.close()
+    exportFile.close()
     tempFile.close()
 
 # define generate
-def generate(numFirst, numLast, maleFirstFile, femaleFirstFile, lastFile, tempFile, which_file):
-    files_first = [maleFirstFile, femaleFirstFile]
+def generate(numFirst, numLast, listOpenFiles, lastFile, tempFile, which_file, delimiter):
     for i in range(0, int(numFirst)):
-        first = which_file(files_first).readline().split(' ')[0]
+        first = which_file(listOpenFiles).readline().split(' ')[0]
         for j in range(0, int(numLast)):
             last = lastFile.readline().split(' ')[0]
-            tempFile.write(first + " " + last + "\n")
+            tempFile.write(first + delimiter + last + "\n")
         lastFile.seek(0)
 
 def random_file(thisList):
@@ -82,9 +91,17 @@ def build_parser():
     parser = ArgumentParser()
 
     parser.add_argument("-m", "--male", action = "store_true", help="male first names")
-    parser.add_argument("-f", "--female", action = "store_true", help = "female first names" )
+    parser.add_argument("-f", "--female", action = "store_true", help = "female first names")
+
+    parser.add_argument("-t", "--text", action = "store_true", help = "text file")
 
     return parser
+
+# # # # # # # # # # # # # # # # # # #
+#                                   #
+#               Run                 #
+#                                   #
+# # # # # # # # # # # # # # # # # # #
 
 # execute
 if __name__ == "__main__":
